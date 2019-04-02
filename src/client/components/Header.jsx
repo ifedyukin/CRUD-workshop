63208e02ada1fa1bcf77a6b8683142f33d02f5ea
Igor_Fedyukin <igor_fedyukin@epam.com>
2019-04-02 15:03:15 +0300

    Add logout button

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import Cookie from 'universal-cookie';
 import { Link } from 'react-router-dom';
+
+const cookie = new Cookie();
 
 export const Header = ({ title, subtitle }) => (
   <header>
@@ -7,5 +10,11 @@ export const Header = ({ title, subtitle }) => (
       {title ? <div className="title"><Link to="/">{title}</Link></div> : null}
       {subtitle ? <div className="subtitle">{subtitle}</div> : null}
     </div>
+    <button className="header-logout" onClick={() => {
+      cookie.remove('token');
+      window.location = '';
+    }}>
+      Logout
+    </button>
   </header>
 );
